2ae2fb41ca10ccd5ce7fb13598eabdc0237fbaac
BharathiMeda <128385999+BharathiMeda@users.noreply.github.com>
2025-05-14 20:32:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
  provider "vsphere" {
-  user           = "vmadmin@dc.com"
-  password       = "Password@123"
-  vsphere_server = "vcsa01.dc.com"
+  user           = "medasb2!"
+  password       = "Challakere@577522"
+  vsphere_server = "vcf-achi1-w1-vc.corp.medtronic.com"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
